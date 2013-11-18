b0a82691713eec9bce293406703790a075881a0c
Adrian Medina <adrian.medina@mail.yu.edu>
2013-11-18 00:20:41 -0500

    init

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject cljs.core.logic "0.1.0-SNAPSHOT"
+  :description ""
+  :url ""
+  :license {:name ""
+            :url ""}
+  :dependencies [[org.clojure/clojure "1.6.0-alpha2"]
+                 [org.clojure/clojurescript "0.0-2030"]
+                 [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]
+                 [org.clojure/tools.namespace "0.2.4"]
+                 [riddley "0.1.6"]]
+  :source-paths ["dev" "src"]
+  :jvm-opts ["-Xmx1g" "-server"]
+  :plugins [[com.cemerick/austin "0.1.3"]
+            [com.cemerick/clojurescript.test "0.2.1"]])
---
+++ src/cljs/core/logic/nominal.clj
@@ -0,0 +1,17 @@
+(ns cljs.core.logic.nominal)
+
+(defn- nom-bind [sym]
+  ((juxt identity
+         (fn [s] `(nom (cljs.core.logic/lvar '~s)))) sym))
+
+(defn- nom-binds [syms]
+  (mapcat nom-bind syms))
+
+(defmacro fresh
+  "Creates fresh noms. Goals occuring within form a logical conjunction."
+  [[& noms] & goals]
+  `(fn [a#]
+     (cljs.core.logic/-inc
+      (let [~@(nom-binds noms)]
+        (cljs.core.logic/bind* a# ~@goals)))))
+
