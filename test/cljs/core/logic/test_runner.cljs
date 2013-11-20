a4973f3f5fd71fb62e2ee7970a02203bf3341381
Adrian Medina <adrian.medina@mail.yu.edu>
2013-11-19 21:54:39 -0500

    added test runner class to just quickly run all tests

---
+++ test/cljs/core/logic/test_runner.cljs
@@ -0,0 +1,8 @@
+(ns cljs.core.logic.test-runner
+  (:require [cemerick.cljs.test :as t])
+  (:require-macros [cemerick.cljs.test :refer [deftest run-tests is testing]]))
+
+(t/test-ns 'cljs.core.logic.test)
+(t/test-ns 'cljs.core.logic.test-extras)
+(t/test-ns 'cljs.core.logic.nominal.tests)
+(t/test-ns 'cljs.core.logic.pldb.tests)
